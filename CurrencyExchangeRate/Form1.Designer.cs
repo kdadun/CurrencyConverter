03f3cad759fadee284e89eaaeb2d7ee7133baf33
Kamil <kamil.dadun@interia.pl>
2017-09-18 23:47:39 +0200

    Currency exchange rate converter

---
+++ CurrencyExchangeRate/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace CurrencyExchangeRate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioEnglish = new System.Windows.Forms.RadioButton();
+            this.radioPolish = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLanguage = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(214, 162);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(214, 213);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(211, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 42);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioEnglish
+            // 
+            this.radioEnglish.AutoSize = true;
+            this.radioEnglish.Location = new System.Drawing.Point(19, 27);
+            this.radioEnglish.Name = "radioEnglish";
+            this.radioEnglish.Size = new System.Drawing.Size(59, 17);
+            this.radioEnglish.TabIndex = 6;
+            this.radioEnglish.TabStop = true;
+            this.radioEnglish.Text = "English";
+            this.radioEnglish.UseVisualStyleBackColor = true;
+            // 
+            // radioPolish
+            // 
+            this.radioPolish.AutoSize = true;
+            this.radioPolish.Location = new System.Drawing.Point(19, 51);
+            this.radioPolish.Name = "radioPolish";
+            this.radioPolish.Size = new System.Drawing.Size(53, 17);
+            this.radioPolish.TabIndex = 7;
+            this.radioPolish.TabStop = true;
+            this.radioPolish.Text = "Polish";
+            this.radioPolish.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(14, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(14, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(14, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(14, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLanguage);
+            this.groupBox1.Controls.Add(this.radioEnglish);
+            this.groupBox1.Controls.Add(this.radioPolish);
+            this.groupBox1.Location = new System.Drawing.Point(520, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 156);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnLanguage
+            // 
+            this.btnLanguage.Location = new System.Drawing.Point(30, 94);
+            this.btnLanguage.Name = "btnLanguage";
+            this.btnLanguage.Size = new System.Drawing.Size(123, 35);
+            this.btnLanguage.TabIndex = 13;
+            this.btnLanguage.Text = "Choose language";
+            this.btnLanguage.UseVisualStyleBackColor = true;
+            this.btnLanguage.Click += new System.EventHandler(this.btnLanguage_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(45, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 13;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(403, 268);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(384, 223);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 509);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Currency converter";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioEnglish;
+        private System.Windows.Forms.RadioButton radioPolish;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLanguage;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ CurrencyExchangeRate/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace CurrencyExchangeRate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadCurrency();
+            LoadCurrencyIntoTable();
+            
+
+        }
+        public void LoadCurrency()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNodeList nodeList;
+            doc.Load("http://api.nbp.pl/api/exchangerates/tables/A/?format=xml");
+            nodeList = doc.GetElementsByTagName("Rate");
+            for (int i = 1; i < nodeList.Count; i++)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Text = nodeList[i].FirstChild.InnerText;
+                item.Value = nodeList[i].ChildNodes.Item(2).InnerText;
+                comboBox1.Items.Add(item);
+                comboBox2.Items.Add(item);
+            }
+        }
+        public void LoadCurrencyIntoTable()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNodeList nodeList;
+            doc.Load("http://api.nbp.pl/api/exchangerates/tables/A/?format=xml");
+
+            DataTable table = new DataTable();
+            table.Columns.Add("Waluta", typeof(string));
+            table.Columns.Add("Kod", typeof(string));
+            table.Columns.Add("Wartość", typeof(string));
+            
+            nodeList = doc.GetElementsByTagName("Rate");
+            for (int i = 1; i < nodeList.Count; i++)
+            {
+                DataRow row = table.NewRow();
+                row[0] = nodeList[i].FirstChild.InnerText;
+                row[1] = nodeList[i].ChildNodes.Item(1).InnerText;
+                row[2] = nodeList[i].ChildNodes.Item(2).InnerText;
+
+                table.Rows.Add(row);
+            }
+            dataGridView1.DataSource = table;
+        }
+       
+        public void EchangeRate(double yourInput,double combo1,double combo2)
+        {
+            double result = Math.Round(((yourInput * combo1) / combo2),2);
+            label1.Text = result.ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double combo1,combo2,textbox;
+            
+            try
+            {
+                if (radioEnglish.Checked == true || radioPolish.Checked == true)
+                {
+                    if (double.TryParse(((comboBox1.SelectedItem as ComboBoxItem).Value).ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out combo1) && double.TryParse(textBox1.Text, out textbox) && double.TryParse(((comboBox2.SelectedItem as ComboBoxItem).Value).ToString(), NumberStyles.Any, CultureInfo.InvariantCulture, out combo2))
+                    {
+                        EchangeRate(textbox, combo1, combo2);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid input");
+                    }
+                }
+                else
+                {
+
+                    MessageBox.Show("Choose language");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void btnLanguage_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlNodeList nodeDate;
+            doc.Load("http://api.nbp.pl/api/exchangerates/tables/A/?format=xml");
+            nodeDate = doc.GetElementsByTagName("EffectiveDate");
+            for (int j = 0; j < nodeDate.Count; j++)
+            {
+                if (radioEnglish.Checked == true)
+                {
+                    button1.Text = "Convert ";
+                    label2.Text = "Enter the amount of money:";
+                    label3.Text = "Select currency:";
+                    label4.Text = "Convert to:";
+                    label5.Text = "Value after conversion";
+                    label6.Text = "Based on quotations of foreign exchange rates of the NBP from " + nodeDate[0].InnerText;
+                   
+                }
+                else if (radioPolish.Checked == true)
+                {
+                    button1.Text = "Przelicz";
+                    btnLanguage.Text = "Wybierz język";
+                    label2.Text = "Wprowadz kwotę pieniędzy:";
+                    label3.Text = "Przelicz z:";
+                    label4.Text = "Przelicz na:";
+                    label5.Text = "Wartoś po konwersji";
+                    label6.Text = "Na podstawie notowań kursów walut obcych NBP z " + nodeDate[0].InnerText;
+                    
+                    
+                    
+                }
+            }
+        }
+    }
+}
